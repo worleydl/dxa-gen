eabc5ed71cfaef8ed29befdbbcde7fdcae9b409d
Dan Worley <danworley@gmail.com>
2026-03-10 12:52:54 -0400

    Init commit

---
+++ src/adlmidi2ogg.cpp
@@ -0,0 +1,140 @@
+#include <adlmidi.h>
+
+#include <vorbis/vorbisenc.h>
+#include <ogg/ogg.h>
+
+#include <fstream>
+#include <vector>
+#include <iostream>
+
+// Generated with GPT
+int main()
+{
+    const char* midiFile = "descent.mid";
+    const char* outFile  = "descent.ogg";
+
+    const int sampleRate = 48000;
+    const int channels   = 2;
+
+    // -------------------------
+    // Init ADLMIDI
+    // -------------------------
+    ADL_MIDIPlayer* player = adl_init(sampleRate);
+    if(!player)
+    {
+        std::cerr << "Failed to init ADLMIDI\n";
+        return 1;
+    }
+
+    if(adl_openFile(player, midiFile) < 0)
+    {
+        std::cerr << "Failed to load MIDI\n";
+        return 1;
+    }
+
+    adl_setBank(player, 4);
+    adl_setNumChips(player, 2);
+
+    // -------------------------
+    // Init Vorbis encoder
+    // -------------------------
+    vorbis_info vi;
+    vorbis_info_init(&vi);
+
+    float quality = 0.5f;
+
+    if(vorbis_encode_init_vbr(&vi, channels, sampleRate, quality))
+    {
+        std::cerr << "Vorbis init failed\n";
+        return 1;
+    }
+
+    vorbis_comment vc;
+    vorbis_comment_init(&vc);
+    vorbis_comment_add_tag(&vc, "ENCODER", "libADLMIDI example");
+
+    vorbis_dsp_state vd;
+    vorbis_analysis_init(&vd, &vi);
+
+    vorbis_block vb;
+    vorbis_block_init(&vd, &vb);
+
+    ogg_stream_state os;
+    ogg_stream_init(&os, rand());
+
+    std::ofstream out(outFile, std::ios::binary);
+
+    // -------------------------
+    // Write Vorbis headers
+    // -------------------------
+    ogg_packet header;
+    ogg_packet header_comm;
+    ogg_packet header_code;
+
+    vorbis_analysis_headerout(&vd, &vc, &header, &header_comm, &header_code);
+
+    ogg_stream_packetin(&os, &header);
+    ogg_stream_packetin(&os, &header_comm);
+    ogg_stream_packetin(&os, &header_code);
+
+    ogg_page og;
+    while(ogg_stream_flush(&os, &og))
+    {
+        out.write((char*)og.header, og.header_len);
+        out.write((char*)og.body, og.body_len);
+    }
+
+    // -------------------------
+    // Render + encode loop
+    // -------------------------
+    const int BUFFER = 1024;
+    std::vector<short> pcm(BUFFER * channels);
+
+    while(adl_play(player, BUFFER, pcm.data()) > 0)
+    {
+        float **buffer = vorbis_analysis_buffer(&vd, BUFFER);
+
+        for(int i=0;i<BUFFER;i++)
+        {
+            buffer[0][i] = pcm[i*2] / 32768.f;
+            buffer[1][i] = pcm[i*2+1] / 32768.f;
+        }
+
+        vorbis_analysis_wrote(&vd, BUFFER);
+
+        while(vorbis_analysis_blockout(&vd, &vb) == 1)
+        {
+            vorbis_analysis(&vb, nullptr);
+            vorbis_bitrate_addblock(&vb);
+
+            ogg_packet op;
+            while(vorbis_bitrate_flushpacket(&vd, &op))
+            {
+                ogg_stream_packetin(&os, &op);
+
+                while(ogg_stream_pageout(&os, &og))
+                {
+                    out.write((char*)og.header, og.header_len);
+                    out.write((char*)og.body, og.body_len);
+                }
+            }
+        }
+    }
+
+    vorbis_analysis_wrote(&vd, 0);
+
+    // -------------------------
+    // Cleanup
+    // -------------------------
+    out.close();
+
+    ogg_stream_clear(&os);
+    vorbis_block_clear(&vb);
+    vorbis_dsp_clear(&vd);
+    vorbis_comment_clear(&vc);
+    vorbis_info_clear(&vi);
+
+    adl_close(player);
+
+    std::cout << "Finished writing OGG\n";
+}
